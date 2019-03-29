afc1075165f464e278cd23182a4addc123ed4b1a
Enhbold <31661625+EnhboldH@users.noreply.github.com>
2019-03-30 00:41:49 +0800

    updated
    fixed

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-import os
-import sys
+import os,sys
+import getpass
 
-walk_dir = sys.argv[1]
 
-print('walk_dir = ' + walk_dir)
+user = getpass.getuser()
 
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+filepath = "C:\\Users\\" + user + "\\AppData\\Local\\osu!\\Songs"
 
-for root, subdirs, files in os.walk(walk_dir):
-    print('--\nroot = ' + root)
-    list_file_path = os.path.join(root, 'my-directory-list.txt')
-    print('list_file_path = ' + list_file_path)
-
-    with open(list_file_path, 'wb') as list_file:
-        for subdir in subdirs:
-            print('\t- subdirectory ' + subdir)
-
-        for filename in files:
-            file_path = os.path.join(root, filename)
-
-            print('\t- file %s (full path: %s)' % (filename, file_path))
-
-            with open(file_path, 'rb') as f:
-                f_content = f.read()
-                list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-                list_file.write(f_content)
-                list_file.write(b'\n')
+for dirName, subdirList, fileList in os.walk(filepath):
+    print(dirName)
+    for fname in fileList:
+        if ".osu" in fname:
+            with open(str(dirName)+ str('\\') +str(fname), "r+") as f:
+                d = f.readlines()
+                f.seek(0)
+                for i in d:
+                    if "ApproachRate" not in i:
+                        f.write(i)
+                    if "OverallDifficulty" in i:
+                        f.write("ApproachRate:10\n")
+                f.truncate()
